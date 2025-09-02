b0978ad90e34a51c9862f46e52af6fe406d93db7
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-02 17:31:04 -0400

    Contexto creado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RegistroJugadores;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Jugadores> Jugadores { get; set; }
+}
