c66e1a19d89342bc4890f8151b9e57ce748fc75d
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-02 18:06:34 -0400

    Método "Insertar" agregado

--- Services/JugadoresService.cs
+++ Services/JugadoresService.cs
@@ -10,6 +10,10 @@ public class JugadoresService(IDbContextFactory<Contexto> DbFactory)
         return await contexto.Jugadores.AnyAsync(j => j.JugadorId == jugadorId);
     }
 
-
-
+    private async Task<bool> Insertar(Jugadores jugador)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Jugadores.Add(jugador);
+        return await contexto.SaveChangesAsync() > 0;
+    }
 }
