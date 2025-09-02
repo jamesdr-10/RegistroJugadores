230db702d65b3cbd8508a98eb031497fc87f42ac
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-02 17:28:51 -0400

    Modelo creado (clase Jugadores)

---
+++ Models/Jugadores.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroJugadores;
+
+public class Jugadores
+{
+    [Key]
+    public int JugadorId { get; set; }
+
+    [Required(ErrorMessage = "El nombre no puede estar vacío")]
+    public string Nombre { get; set; } = null!;
+
+    [Range(0, int.MaxValue, ErrorMessage = "La cantidad de partidas jugadas debe ser mayor o igual que cero")]
+    public int PartidasJugadas { get; set; }
+}
