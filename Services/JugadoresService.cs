65dba5a1de5abea92eaeafdf0847abf4ae7b67de
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-02 18:03:01 -0400

    Método Existe agregado

---
+++ Services/JugadoresService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RegistroJugadores;
+
+public class JugadoresService(IDbContextFactory<Contexto> DbFactory)
+{
+    private async Task<bool> Existe(int jugadorId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Jugadores.AnyAsync(j => j.JugadorId == jugadorId);
+    }
+
+
+
+}
