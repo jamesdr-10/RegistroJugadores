c9c685e14812691073bb41d8798491fe1b83a1df
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-10 18:00:58 -0400

    Modelo Partidas agregada al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,6 +6,7 @@ namespace RegistroJugadores.DAL;
 public class Contexto : DbContext
 {
     public DbSet<Jugadores> Jugadores { get; set; }
+    public DbSet<Partidas> Partidas { get; set; }
 
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
