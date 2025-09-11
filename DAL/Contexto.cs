a6b86cd74043ae899606da956f6c22702d8d5693
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-10 22:34:47 -0400

    OnModelCreating añadido al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,4 +9,33 @@ public class Contexto : DbContext
     public DbSet<Partidas> Partidas { get; set; }
 
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Partidas>()
+            .HasOne(p => p.Jugador1)
+            .WithMany()
+            .HasForeignKey(p => p.Jugador1Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+            .HasOne(p => p.Jugador2)
+            .WithMany()
+            .HasForeignKey(p => p.Jugador2Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+            .HasOne(p => p.Ganador)
+            .WithMany()
+            .HasForeignKey(p => p.GanadorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+            .HasOne(p => p.TurnoJugador)
+            .WithMany()
+            .HasForeignKey(p => p.TurnoJugadorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
