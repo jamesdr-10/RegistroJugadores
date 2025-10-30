fe3c5c7a438a03148e41dbc7425bea58590672dd
jamesdr-10 <james_depena@ucne.edu.do>
2025-10-30 10:28:09 -0400

    Creando api services

---
+++ Services/PartidasApiService.cs
@@ -0,0 +1,22 @@
+﻿namespace RegistroJugadores;
+
+public interface IPartidasApiService
+{
+    Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
+}
+
+public class PartidasApiService(HttpClient httpClient) : IPartidasApiService
+{
+    public async Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId)
+    {
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<PartidaResponse>($"api/Partidas/{partidaId}");
+            return new Resource<PartidaResponse>.Success(response!);
+        }
+        catch (Exception ex)
+        {
+            return new Resource<PartidaResponse>.Error(ex.Message);
+        }
+    }
+}
